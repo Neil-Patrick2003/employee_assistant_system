f4cd3f573cf92b32bc2f41d465f0990166d65a91
Neil Patrick <neilpatrick.personal@gmail.com>
2024-11-24 21:06:28 +0800

    Auto stash before checking out "origin/main"

---
+++ resources/views/applicant/profile/index.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <div class="py-4">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+            <div class="container mx-auto p-4">
+                <!-- Grid Container -->
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <!-- Grid Items -->
+                    <div class=" sm:col-span-1 bg-white overflow-hidden p-2 sm:rounded-lg">Item 1</div>
+                    <div class="col-span-3 overflow-hidden  p-2 sm:rounded-lg">
+                        <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+                            <!-- Grid Items -->
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 1</div>
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 2</div>
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 1</div>
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 2</div>
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 1</div>
+                            <div class="col-span-2 bg-white overflow-hidden shadow-sm p-2 sm:rounded-lg">Item 2</div>
+                            <div class="col-span-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div class="p-6 text-gray-900">
+                                    Exploore
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-app-layout>
