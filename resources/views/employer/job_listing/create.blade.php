3f8fcebabeeedee44e3f888ff376b37c46dd9d4b
Neil Patrick <neilpatrick.personal@gmail.com>
2024-11-26 22:33:33 +0800

    fix-job-posting-form

--- resources/views/employer/job_listing/create.blade.php
+++ resources/views/employer/job_listing/create.blade.php
@@ -44,7 +44,8 @@
     </div>
 
 
-    <div class="mx-auto max-w-7xl sm:px-6 px-2 py-6  lg:px-8 mt-4 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+    <div
+        class="mx-auto max-w-7xl sm:px-6 px-2 py-6  lg:px-8 mt-4 bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
         <form action="/employer/jobs/create" method="POST">
             @csrf
             <div class="space-y-12">
@@ -57,11 +58,11 @@
                     </div>
 
                     <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
-                        <div class="sm:col-span-4">
+                        <div class="col-span-full">
                             <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                             <div class="mt-2">
                                 <div
-                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
                                     <input type="text" name="title" id="title"
                                         class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6">
                                 </div>
@@ -88,7 +89,7 @@
                             @enderror
                         </div>
 
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
                             <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
                             <div class="mt-2">
                                 <div
@@ -104,7 +105,7 @@
                             </div>
                         </div>
 
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
                             <label for="location" class="block text-sm/6 font-medium text-gray-900">Location</label>
                             <div class="mt-2">
                                 <div
@@ -120,7 +121,7 @@
                             </div>
                         </div>
 
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
                             <label for="work_policy" class="block text-sm/6 font-medium text-gray-900">Work
                                 Policy</label>
                             <div class="mt-2">
@@ -137,7 +138,7 @@
                                 </div>
                             @enderror
                         </div>
-                        <div class="sm:col-span-4">
+                        <div class="sm:col-span-3">
                             <label for="category" class="block text-sm/6 font-medium text-gray-900">
                                 category</label>
                             <div class="mt-2">
@@ -164,7 +165,7 @@
                     </div>
 
                     <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
-                        <div class="sm:col-span-3">
+                        <div class="sm:col-span-2">
                             <label for="min_age" class="block text-sm/6 font-medium text-gray-900">Minimum
                                 Age</label>
                             <div class="mt-2">
@@ -178,7 +179,7 @@
                             @enderror
                         </div>
 
-                        <div class="sm:col-span-3">
+                        <div class="sm:col-span-2">
                             <label for="max_age" class="block text-sm/6 font-medium text-gray-900">Maximum
                                 Age</label>
                             <div class="mt-2">
@@ -192,11 +193,11 @@
                             @enderror
                         </div>
 
-                        <div class="sm:col-span-3">
+                        <div class="sm:col-span-2">
                             <label for="work_experience" class="block text-sm/6 font-medium text-gray-900">
                                 Work Experience (in years)</label>
                             <div class="mt-2">
-                                <input type="text" name="work_experience" id="wprk_experience"
+                                <input type="number" name="work_experience" id="wprk_experience"
                                     autocomplete="family-name"
                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
                             </div>
@@ -229,28 +230,37 @@
 
                     <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
                         <div class="sm:col-span-3 relative">
-                            <label for="min_age" class="block text-sm/6 font-medium text-gray-900">Add
-                                Skill</label>
+                            <label for="min_age" class="flex items-center text-sm font-medium text-gray-900">
+                                Add Skills
+                                <button type="button" id="add-btn"
+                                    class="ml-auto text-indigo-600 hover:text-indigo-800">
+                                    <svg class="w-6 h-6 text-gray-800" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </label>
 
-                            <button type="button" id="add-btn"
-                                class="absolute top-0 right-0 mt-1 mr-2 text-indigo-600 hover:text-indigo-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor" class="h-6 w-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 4v16m8-8H4" />
-                                </svg>
-                            </button>
+
+
+
 
                             <div class="mt-2 input-group" id="input-container">
                                 <div class="input-item flex items-center">
                                     <input type="text" name="job_skills[]"
                                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
                                     <button type="button" class="remove-btn text-red-600 hover:text-red-800 ml-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke="currentColor" class="h-5 w-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M6 18L18 6M6 6l12 12" />
+                                        <svg class="w-6 h-6 text-gray-800" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd"
+                                                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z"
+                                                clip-rule="evenodd" />
                                         </svg>
+
                                     </button>
                                 </div>
                             </div>
@@ -319,8 +329,8 @@
             newInputGroup.innerHTML = `
                 <input type="text" name="job_skills[]" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6">
                 <button type="button" class="remove-btn text-red-600 hover:text-red-800 ml-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z" clip-rule="evenodd"/>
                     </svg>
                 </button>
             `;
