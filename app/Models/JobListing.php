86f060d6dd49632ffef76d30c889184ee8875740
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2024-12-01 21:27:17 +0800

    fix

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -58,26 +58,32 @@ class JobListing extends Model
 
         return $builder->where(function (Builder $query) use ($user) {
             $query
+                // Get jobs where minimum age is less than the user age and maximum_age is greater than user age
                 ->where(function (Builder $subQuery) use ($user) {
                     $subQuery->where('minimum_age', '<=', $user->age)
                         ->where('maximum_age', '>=', $user->age);
                 })
+                // if the user job preference category is set. find jobs only that has the user preferred category
                 ->when($user->job_preference->category,
                     function (Builder $subQuery) use ($user) {
                         $subQuery->orWhere('category', $user->job_preference->category);
                 })
+                // if the user job preference scope is set. find jobs only that has the user preferred scope
                 ->when($user->job_preference->scope,
                     function (Builder $subQuery) use ($user) {
                         $subQuery->orWhere('work_policy', $user->job_preference->scope);
                     })
+                // if the user job preference work_policy is set. find jobs only that has the user preferred work_policy
                 ->when($user->job_preference->work_policy,
                     function (Builder $subQuery) use ($user) {
                         $subQuery->orWhere('work_policy', $user->job_preference->work_policy);
                     })
+                // if the user job preference work_experience is set. find jobs only that has requierd_work_experience that is less than user work_experience
                 ->when($user->job_preference->work_experience,
                     function (Builder $subQuery) use ($user) {
                         $subQuery->orWhere('requierd_work_experience', '<=', $user->job_preference->work_experience);
                     })
+                // if the user job preference min_salary is set. find jobs only that has salary that is greater than user preferred min_salary
                 ->when($user->job_preference->min_salary,
                     function (Builder $subQuery) use ($user) {
                         $subQuery->orWhere('salary', '>=', $user->job_preference->min_salary);
