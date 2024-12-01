88a074eb132348b42e168f581f3d1a126cc82a9b
Neil Patrick <neilpatrick.personal@gmail.com>
2024-12-01 21:23:27 +0800

    admin-job-manage

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\JobListing;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class JobController extends Controller
 {
     public function index(){
-        return view('admin.job.index');
+
+        $jobs = JobListing::with('company')
+        ->orderBy('title', 'asc')
+        ->get();
+
+
+        return view('admin.job.index', [
+            'jobs' => $jobs
+        ]);
     }
 }
--- resources/views/admin/job/index.blade.php
+++ resources/views/admin/job/index.blade.php
@@ -1,3 +1,54 @@
 <x-admin-layout>
-    Job management
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-base font-semibold text-gray-900">Jobs</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all the jobs </p>
+            </div>
+        </div>
+        <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th scope="col"
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Title
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Company</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Status</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200">
+                            @foreach ($jobs as $job)
+                                <tr>
+                                    <td
+                                        class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                        {{$job->title}}</td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                        {{$job->company->name}}
+                                    </td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$job->status}}</td>
+                                    <td
+                                        class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span
+                                                class="sr-only">, Lindsay Walton</span></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+
+                            <!-- More people... -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-admin-layout>
