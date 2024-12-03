c757d7dff6fb464162ce5f107d731be97d25799b
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2024-12-03 09:20:40 +0800

    add database seedr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@peso-jobs.byandev.com',
+            'role' => 'Admin',
+            'password' => Hash::make('9F)b4Hny£2'),
+            'age' => 30,
+            'email_verified_at' => now()
         ]);
     }
 }
